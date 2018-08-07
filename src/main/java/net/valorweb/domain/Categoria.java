35f554c5d63be56bfa398486660cebe3401258b0
Felipe de Souza Santos <FO005992@intermedium.local>
2018-08-07 16:46:36 -0300

    ajuste Lombok

--- src/main/java/net/valorweb/domain/Categoria.java
+++ src/main/java/net/valorweb/domain/Categoria.java
@@ -8,7 +8,16 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of="id")
 public class Categoria implements Serializable {
 
 	/**
@@ -16,62 +25,16 @@ public class Categoria implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public Categoria() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public Categoria(Integer id, String nome) {
-		super();
-		this.id = id;
-		this.nome = nome;
-	}
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Getter @Setter 
 	private Integer id;
 
 	@Column
+	@Getter @Setter
 	private String nome;
 
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Categoria other = (Categoria) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
+	
 
 }
