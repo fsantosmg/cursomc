c85fb63de703365cfc2165ab9c717aa03d6dbbae
Felipe de Souza Santos <felipe@localhost.localdomain>
2018-09-05 22:07:41 -0300

    inserindo nova categoria

--- src/main/java/net/valorweb/recources/CategoriaResource.java
+++ src/main/java/net/valorweb/recources/CategoriaResource.java
@@ -1,13 +1,17 @@
 package net.valorweb.recources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import net.valorweb.domain.Categoria;
 import net.valorweb.services.CategoriaService;
@@ -30,6 +34,15 @@ public class CategoriaResource {
 		
 		return ResponseEntity.ok().body(service.buscar(id));
 		
+	}
+	
+	
+	@PostMapping
+	public ResponseEntity<Void> insert (@RequestBody Categoria categoria){
+		
+		URI uri =  ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(service.insert(categoria).getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 }
