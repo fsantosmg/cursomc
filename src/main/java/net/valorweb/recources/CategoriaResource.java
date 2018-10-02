06c4c56a81e2e7d9b2bbaf85d92b9fa47d96f4c6
Felipe Santos <fsantosmg@gmail.com>
2018-10-02 06:46:03 -0300

    Autorizando endpoints para perfis especificos

--- src/main/java/net/valorweb/recources/CategoriaResource.java
+++ src/main/java/net/valorweb/recources/CategoriaResource.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -51,6 +52,7 @@ public class CategoriaResource {
 	}
 
 	@PostMapping
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO categoriaDTO) {
 
 		Categoria categoria = service.fromDTO(categoriaDTO);
@@ -60,6 +62,7 @@ public class CategoriaResource {
 	}
 
 	@PutMapping("/{id}")
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO categoriaDTO, @PathVariable Integer id) {
 
 		Categoria categoria = service.fromDTO(categoriaDTO);
@@ -73,6 +76,7 @@ public class CategoriaResource {
 	}
 
 	@DeleteMapping("/{id}")
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 
 		service.delete(id);
